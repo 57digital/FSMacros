7cda031ce1f8600fbaa34e07bbd75b57e73a69eb
luke.dixon <luke.dixon@57digital.co.uk>
2014-02-13 15:51:38 +0000

    Added FSMacros.h and updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ FSMacros
 ========
 
 A collection of macros to make Objective-C development easier.
+
+###Usage
+
+Copy **FSMacros.h** into your Xcode project and include it: `#import  "FSMacros.h"`.
+
+###Macros included
+
+Look into **FSMacro.h** for full list of available macros.
---
+++ Source/FSMacros.h
@@ -0,0 +1,14 @@
+//
+//  FSMacros.h
+//
+//  Created by Luke Dixon on 09/01/2014.
+//  Copyright (c) 2014 57Digital. All rights reserved.
+//
+
+#ifndef SkinEditor_Macros_h
+#define SkinEditor_Macros_h
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromHex(rgbValue) UIColorFromRGB(rgbValue)
+
+#endif
